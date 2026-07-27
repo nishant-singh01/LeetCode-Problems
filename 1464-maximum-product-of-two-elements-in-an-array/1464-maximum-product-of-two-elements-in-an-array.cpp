3db305ca626a18c8af91ca3fc6d17b689a70e55b
Nishant Singh <nsingh012006@gmail.com>
2026-07-27 21:42:09 +0530

    Optimize maxProduct function for better performance
    Refactored maxProduct function to use an optimal approach for finding the maximum product of two elements.

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,10 +1,23 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
+//-----Sorting-Based Approach-----
 
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        return (nums[n-1]-1)*(nums[n-2]-1);
+    
+int n=nums.size();
+sort(nums.begin(),nums.end());
 
+return (nums[n-1]-1)*(nums[n-2]-1);
+
+
+
+// -----Optimal Approach-----
+int first = 0, second = 0;
+
+for (int num : nums) {
+    if (num > first) {
+        second = first;
+        first = num;
+    } else if (num > second) {
+        second = num;
     }
-};
+}
+
+return (first - 1) * (second - 1);
